5a67c19f3de9a393c13eef2ccb8dc5cbdd1177f9
Laszlo Havasi <b05xf0@inf.elte.hu>
2023-06-17 11:09:10 +0200

    Clean module

--- app/Eval/Solution.hs
+++ app/Eval/Solution.hs
@@ -30,32 +30,3 @@ mkScore ts = sum $ ET.score <$> ts
 
 mkMaxScore :: [ET.Task] -> Int
 mkMaxScore ts = sum $ ET.maxScore <$> ts
-
-
-{-
-{-# LANGUAGE OverloadedStrings #-}
-
-module Eval.Solution where
-
-import Prelude hiding ( unlines )
-
-import Data.Text ( Text, pack, unlines )
-import qualified Eval.Task as ET
-import qualified Data.Solution as DS
-import qualified Data.Tasklist as DTL
-
-data Solution = Solution
-  {
-    details :: DS.Solution,
-    evals :: [ET.Task]
-  } deriving (Show)
-
-score :: Solution -> Int
-score s = sum . map ET.score $ evals s
-
-maxScore :: Solution -> Int
-maxScore s = sum . map ET.maxScore $ evals s
-
-result :: Solution -> Text
-result s = (pack . show $ score s) <> "/" <> (pack . show $ maxScore s) <> " points"
--}
